5c2a47878bab254a4a4c05baf3e7ebd9b4b251fc
Milstein Munakami <milsonmun@yahoo.com>
2018-06-12 17:08:01 -0500

    Use generic views: Less code is better

--- polls/urls.py
+++ polls/urls.py
@@ -4,13 +4,21 @@ from . import views
 
 app_name = 'polls'
 
-urlpatterns = [
+# urlpatterns = [
     # /polls/
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
     # /polls/1
-    path('<int:question_id>/', views.detail, name='detail'),
+    #path('<int:question_id>/', views.detail, name='detail'),
     # /polls/1/vote
-    path('<int:question_id>/vote', views.vote, name='vote'),
+    #path('<int:question_id>/vote', views.vote, name='vote'),
     # /polls/1/result
-    path('<int:question_id>/result', views.result, name='result'),
+    #path('<int:question_id>/result', views.result, name='result'),
+#]
+
+urlpatterns=[
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/result/', views.ResultView.as_view(), name='result'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
--- polls/views.py
+++ polls/views.py
@@ -3,9 +3,25 @@ from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_questions'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/result.html'
 
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
