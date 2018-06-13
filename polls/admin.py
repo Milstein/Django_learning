20d0edbfb06278f09cc89c0f5e04f0566731c1df
Milstein Munakami <milsonmun@yahoo.com>
2018-06-13 16:47:04 -0500

    added some admin templating and documentaion Sphinx generated files

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,26 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+# class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+    list_display = ('pub_date', 'question_text', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['choice']
+
+admin.site.register(Question, QuestionAdmin)
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Question, Choice
@@ -17,7 +18,14 @@ class IndexView(generic.ListView):
     # context_object_name = 'latest_questions'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+            Return the last five published questions (not including those set to be
+            published in the future).
+            """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+        # return Question.objects.order_by('-pub_date')[:5]
 
 
 class DeatailView(generic.DetailView):
